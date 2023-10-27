100f916d4481f22b2569e20f3768a01d75284bf4
gusmfs <oigostavo@gmail.com>
2023-10-27 19:16:48 -0300

    entity schedule

--- src/entities/schedules.entity.ts
+++ src/entities/schedules.entity.ts
@@ -1,6 +1,21 @@
-import { Entity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import User from "./users.entity";
 
 @Entity('schedules')
 export default class Schedule{
-    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'date'})
+    date: Date;
+
+    @Column({type: 'time'})
+    hour: string;
+
+    @Column('int')
+    userId: number;
+
+    @ManyToOne(() => User, (u) => u.id)
+    user: User;
 }
+
