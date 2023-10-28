0932f9a66e6a25a41d7b9e5b619c855a832c383c
gusmfs <oigostavo@gmail.com>
2023-10-28 00:49:54 -0300

    feat: realEstates entity

--- src/entities/realEstates.entity.ts
+++ src/entities/realEstates.entity.ts
@@ -1,6 +1,18 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('realEstates')
 export default class RealEstate{
+    @PrimaryGeneratedColumn('increment')
+    id:number 
+    @Column({nullable: false, default : false})
+    sold: boolean
+    @Column({ type: 'decimal', precision: 12, scale: 2, nullable: false, default: 0 })
+    value: number
+    @Column({nullable:false})
+    size: number
+    @CreateDateColumn({type: 'date', nullable: false})
+    createdAt: string
+    @UpdateDateColumn({type: 'date',unique: true, nullable: false})
+    updatedAt: string
     
-}
+}   
