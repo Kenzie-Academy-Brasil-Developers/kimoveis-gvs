1059e3d96155e120ea4c2fcca30a8bbbc9c18140
gusmfs <oigostavo@gmail.com>
2023-10-29 17:10:19 -0300

    l

--- src/entities/categories.entity.ts
+++ src/entities/categories.entity.ts
@@ -5,5 +5,5 @@ export default class Categorie{
     @PrimaryGeneratedColumn('increment')
     id: number
     @Column({length: 45, unique: true, nullable: false})
-    name: string    
+    name: string
 }
