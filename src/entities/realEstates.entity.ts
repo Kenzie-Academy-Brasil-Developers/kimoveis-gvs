76e245a630ae1f727fdf44be63201864c7da0f06
gusmfs <oigostavo@gmail.com>
2023-10-29 23:55:06 -0300

    sad

--- src/entities/realEstates.entity.ts
+++ src/entities/realEstates.entity.ts
@@ -8,6 +8,7 @@ import Categorie from "./categories.entity";
 export default class RealEstate{
     @PrimaryGeneratedColumn('increment')
     id:number 
+    
     @Column({nullable: false, default : false})
     sold: boolean
     @Column({ type: 'decimal', precision: 12, scale: 2, nullable: false, default: 0 })
