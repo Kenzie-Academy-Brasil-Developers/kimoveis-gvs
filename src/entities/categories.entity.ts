82f101eb27ba93b4bedc5226bfdde7b9ca0e010e
gusmfs <oigostavo@gmail.com>
2023-10-29 23:50:00 -0300

    dasujdhau

--- src/entities/categories.entity.ts
+++ src/entities/categories.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import RealEstate from "./realEstates.entity";
 
 @Entity('categories')
 export default class Categorie{
@@ -6,4 +7,6 @@ export default class Categorie{
     id: number
     @Column({length: 45, unique: true, nullable: false})
     name: string
+    @OneToMany(() => RealEstate, (realEstate) => realEstate.category )
+    realEstate: Array<RealEstate>
 }
