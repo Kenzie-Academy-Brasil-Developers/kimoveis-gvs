a3ff25a7099702ab8f9f60c90d53a02440494175
gusmfs <oigostavo@gmail.com>
2023-10-31 19:07:08 -0300

    ausd

---
+++ src/middlewares/schedules.middleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import { Address, RealEstate, Schedule } from "../entities";
+import AppError from "../errors/AppError.error";
+import { schedulesRepo } from "../repositories";
+
+export const verifyRealEstatesExist = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const {realEstateId} = req.body
+    const realEstateExist : RealEstate | null = await realEstateId.findOne({
+        where:{
+            id: Number(realEstateId)
+        }
+    })
+    if(!realEstateExist) throw new AppError('Real Estate not found',404)
+
+    return next()
+}
+
+export const verifyRealEstatesSchedulesExist = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { realEstateId, hour, date} = req.body
+    const schedule = await schedulesRepo.findOne({
+        where: {
+            realEstate : {
+                id : Number(realEstateId)
+            },
+            hour,
+            date
+        }
+    })
+    if(schedule) throw new AppError('Schedule to this Real Estate at this date and time already exists', 409 )
+    return next()
+}
+
+export const verifyUserScheduleExists = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    let {sub} = res.locals.decoded
+    sub = Number(sub)
+    const {hour, date} = req.body
+    const schedule : Schedule | null = await schedulesRepo.findOne({
+        where: {
+            user : {
+                id: sub
+            },
+            date, 
+            hour
+        }
+    }) 
+    if(schedule) throw new AppError('User to this Real Estate at this date and time already exists', 409 )
+    return next()
+}
