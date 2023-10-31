eb60b02336d6d2f96047e745c47ec646c252f986
gusmfs <oigostavo@gmail.com>
2023-10-30 23:10:02 -0300

    categorie repo

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,7 +1,9 @@
 import { Repository } from "typeorm";
 import { RealEstate, Schedule, User } from "./entities";
 import { AppDataSource } from "./data-source";
+import Categorie from "./entities/categories.entity";
 
 export const userRepo : Repository<User> = AppDataSource.getRepository(User)
 export const schedulesRepo : Repository<Schedule> = AppDataSource.getRepository(Schedule)
 export const realEstateRepo : Repository<RealEstate> = AppDataSource.getRepository(RealEstate)
+export const categorieRepo : Repository<Categorie> = AppDataSource.getRepository(Categorie)
