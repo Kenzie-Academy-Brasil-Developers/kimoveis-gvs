30d0367f879104c20baf561166587a2468118e5f
gusmfs <oigostavo@gmail.com>
2023-10-30 23:09:20 -0300

    RE repo

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm";
-import { Schedule, User } from "./entities";
+import { RealEstate, Schedule, User } from "./entities";
 import { AppDataSource } from "./data-source";
 
 export const userRepo : Repository<User> = AppDataSource.getRepository(User)
-export const schedules : Repository<Schedule> = AppDataSource.getRepository(Schedule)
+export const schedulesRepo : Repository<Schedule> = AppDataSource.getRepository(Schedule)
+export const realEstateRepo : Repository<RealEstate> = AppDataSource.getRepository(RealEstate)
