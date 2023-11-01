5ac7bdcb582a030fff470f18bcc7b54232df18fb
gusmfs <oigostavo@gmail.com>
2023-10-31 23:56:58 -0300

    adsa

--- src/middlewares/globals.middleware.ts
+++ src/middlewares/globals.middleware.ts
@@ -12,8 +12,9 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) : v
     const {authorization} = req.headers
 
     if(!authorization) throw new AppError('Missing bearer token', 401)
-    
+
     const token : string = authorization.split(' ')[1]
+    
     const decoded = verify(token, process.env.SECRET_KEY!)
     res.locals = {...res.locals, decoded}
 
