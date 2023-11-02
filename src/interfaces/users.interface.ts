62c7caa00a1e5b733f0ce2832145cb9c424fd286
gusmfs <oigostavo@gmail.com>
2023-11-02 17:43:07 -0300

    dsaudhasu

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,9 +1,14 @@
-import { Repository } from "typeorm";
+import { DeepPartial, Repository } from "typeorm";
 import { User } from "../entities";
 import { z } from "zod";
-import { createUserSchema, updateUserSchema, userWithoutAdmin } from "../schemas/users.schema";
+import { createUserSchema, updateUserSchema, userLoginSchema, userReturnSchema, userWithoutAdmin } from "../schemas/users.schema";
 
 export type UserRepo = Repository<User>
 export type UserCreate = z.infer<typeof createUserSchema>
 export type userWithoutAdmin = z.infer<typeof userWithoutAdmin>
-export type UserUpdate = Omit<UserCreate, 'admin'>
+export type UserBodyUpdate = Omit<UserCreate, 'admin'>
+export type UserUpdate = DeepPartial<UserBodyUpdate>
+export type UserReturn = z.infer<typeof userReturnSchema>
+export type UserLogin = z.infer<typeof userLoginSchema>
+export type LoginReturn = {token : string}
+
