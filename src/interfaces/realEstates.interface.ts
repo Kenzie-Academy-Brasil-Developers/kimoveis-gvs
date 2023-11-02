0a426060eaf5d48faa8042eba8cc0a71b20b2541
gusmfs <oigostavo@gmail.com>
2023-11-02 15:28:04 -0300

    asudhas

--- src/interfaces/realEstates.interface.ts
+++ src/interfaces/realEstates.interface.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { createRealEstateSchema, realEstateSchema } from "../schemas/realEstates.schema";
 import { Repository } from "typeorm";
+import { RealEstate } from "../entities";
 
 export type CreateRealEstate = z.infer<typeof createRealEstateSchema>
-export type RealEstate = Repository<RealEstate>
+export type RealEstateRepo = Repository<RealEstate>
--- src/repositories.ts
+++ src/repositories.ts
@@ -1,9 +1,10 @@
 import { Repository } from "typeorm";
 import { Address, Category, RealEstate, Schedule, User } from "./entities";
 import { AppDataSource } from "./data-source";
+import { RealEstateRepo } from "./interfaces/realEstates.interface";
 
 export const userRepo : Repository<User> = AppDataSource.getRepository(User)
 export const schedulesRepo : Repository<Schedule> = AppDataSource.getRepository(Schedule)
-export const realEstateRepo : Repository<RealEstate> = AppDataSource.getRepository(RealEstate)
+export const realEstateRepo : RealEstateRepo = AppDataSource.getRepository(RealEstate)
 export const categorieRepo : Repository<Category> = AppDataSource.getRepository(Category)
 export const addressRepo : Repository<Address> = AppDataSource.getRepository(Address)
