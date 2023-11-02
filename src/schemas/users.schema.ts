4058c4257857089ff01d8f876bfacb152d64eb25
gusmfs <oigostavo@gmail.com>
2023-11-02 00:30:16 -0300

    asudhas

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -1,12 +1,18 @@
 import { z } from "zod";
 
-export const movieSchema = z.object({
+export const userSchema = z.object({
     id: z.number().positive(),
     name: z.string().max(45).nullish(),
-    email: z.string().max(45),
+    email: z.string().max(45).email(),
     admin: z.boolean().default(false),
     password: z.string().min(1).max(120),
     createdAt: z.date(),
     updatedAt: z.date(),
     deletedAt: z.date().nullable(),
+})
+export const createUserSchema = userSchema.pick({
+    name: true,
+    email: true,
+    admin: true,
+    password: true
 })
