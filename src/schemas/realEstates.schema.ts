5f2fcdc172edbdbb8f61d29d3b6e0b52a909d97d
gusmfs <oigostavo@gmail.com>
2023-11-02 02:15:39 -0300

    asudhas

--- src/schemas/realEstates.schema.ts
+++ src/schemas/realEstates.schema.ts
@@ -2,7 +2,17 @@ import { z } from "zod";
 
 export const realEstateSchema = z.object({
     id: z.number().positive(),
-    sold: z.boolean().default(false),   
-    
-
+    sold: z.boolean().default(false),  
+    value: z.number().default(0),
+    size: z.number(),
+    createdAt: z.string(),
+    updatedAt : z.string(),
+    address: z.object({
+        street: z.string().max(45),
+        zipCode: z.string().max(8),
+        number: z.number(),
+        city: z.string().max(20),
+        state: z.string().max(2)
+    }),
+    categoryId: z.number().positive().int()
 })
