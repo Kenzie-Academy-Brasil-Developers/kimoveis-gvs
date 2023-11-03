26c1e6cc057aa049d9e142a42b01f2f15d5994b3
gusmfs <oigostavo@gmail.com>
2023-11-02 23:17:18 -0300

    feat controller user read

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
-import { createUserService } from "../services/users.service";
+import { createUserService, readAllUsersService } from "../services/users.service";
 
 export const createUserController = async(req: Request, res: Response): Promise<Response> => {
     const user = await createUserService(req.body)
     return res.status(200).json(user)
+}
+export const readAllUsersController = async(req: Request, res: Response): Promise<Response> => {
+    const users = await readAllUsersService()
+    return res.status(200).json(users)
 }
