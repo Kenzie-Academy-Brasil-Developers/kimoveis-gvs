cbbc261d8cafc8d45be9f02c35a710d6e481a9f7
gusmfs <oigostavo@gmail.com>
2023-11-03 18:10:50 -0300

    dasuhdau

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { verifyCategoryExists, verifyUniqueCategoryName } from "../middlewares/categories.middleware";
 import { verifyAdmin, verifyBody, verifyToken } from "../middlewares/globals.middleware";
+import { createCategoryController, readCategoryController, readRealEstatesByCategoryController } from "../controller/category.controller";
 
 export const categoriesRoutes : Router = Router()
-categoriesRoutes.post('/', verifyToken,verifyAdmin,verifyBody,verifyUniqueCategoryName)
-categoriesRoutes.get('/')
-categoriesRoutes.get('/:id/realEstate',verifyCategoryExists)
+categoriesRoutes.post('/', verifyToken,verifyAdmin,verifyBody,verifyUniqueCategoryName, createCategoryController)
+categoriesRoutes.get('/', readCategoryController)
+categoriesRoutes.get('/:id/realEstate',verifyCategoryExists, readRealEstatesByCategoryController)
