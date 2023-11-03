753b39ac7a4da0da2f288e9cad9f234abfef7592
gusmfs <oigostavo@gmail.com>
2023-11-02 22:48:11 -0300

    asudhasu

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyAdmin, verifyBody, verifyPermissions, verifyToken } from "../middlewares/globals.middleware";
 import { verifyUniqueUserEmail, verifyUserExists } from "../middlewares/users.middleware";
+import { createUserController } from "../controller/users.controller";
 
 export const usersRoutes : Router = Router()
-usersRoutes.post('/', verifyBody, verifyUniqueUserEmail)
+usersRoutes.post('/', verifyBody, verifyUniqueUserEmail, createUserController)
 usersRoutes.get('/', verifyToken, verifyAdmin)
 usersRoutes.patch('/:id', verifyBody, verifyToken, verifyUserExists, verifyPermissions)
 usersRoutes.delete('/:id', verifyToken, verifyAdmin, verifyPermissions ,verifyUserExists)
