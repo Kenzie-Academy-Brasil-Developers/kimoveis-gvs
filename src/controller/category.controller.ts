dad2a41d8b218e9bfff4c791d49c2ca0814f0559
gusmfs <oigostavo@gmail.com>
2023-11-03 18:04:45 -0300

    dasuhdau

---
+++ src/controller/category.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { createCategoryService } from "../services/category.service";
+
+export const createCategoryController = async (req: Request, res: Response) : Promise<Response> => {
+    const category = await createCategoryService(req.body)
+
+    return res.status(201).json(category)
+}
