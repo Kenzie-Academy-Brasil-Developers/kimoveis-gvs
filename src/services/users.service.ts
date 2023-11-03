2f102faab9be6fa1de474b617a7438ec0687fde1
gusmfs <oigostavo@gmail.com>
2023-11-03 02:49:45 -0300

    duashdau

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -5,6 +5,7 @@ import { userReadSchema, userReturnSchema } from "../schemas/users.schema";
 
 export const createUserService = async(data: UserCreate ): Promise<UserReturn> => {
     const user : User = userRepo.create(data)
+    
     await userRepo.save(user)
     return userReturnSchema.parse(user) 
 }
