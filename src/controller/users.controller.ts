904893dfd47a2acdc67f7491b4c81fb4cb0bb6b2
gusmfs <oigostavo@gmail.com>
2023-11-03 02:49:14 -0300

    duashdau

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -17,7 +17,8 @@ export const updateUserController = async(req: Request, res: Response): Promise<
 }
 export const softDeleteUserController = async(req: Request, res: Response): Promise<Response> => {
     const {user} = res.locals
-    
+
     await softDeleteUserService(user)
+    
     return res.status(204).json()
 }
