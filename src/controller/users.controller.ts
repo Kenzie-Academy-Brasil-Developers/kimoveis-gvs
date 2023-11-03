8b5b8b51b3924da57b49078830b213adda44a64d
gusmfs <oigostavo@gmail.com>
2023-11-03 02:49:33 -0300

    duashdau

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -3,11 +3,12 @@ import { createUserService, readAllUsersService, softDeleteUserService, updateUs
 
 export const createUserController = async(req: Request, res: Response): Promise<Response> => {
     const user = await createUserService(req.body)
+    
     return res.status(200).json(user)
 }
 export const readAllUsersController = async(req: Request, res: Response): Promise<Response> => {
     const users = await readAllUsersService()
-    
+
     return res.status(200).json(users)
 }
 
