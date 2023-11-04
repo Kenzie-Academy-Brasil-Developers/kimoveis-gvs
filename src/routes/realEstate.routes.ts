440b0a517dde92efa55949fb1ffb457cb0d62feb
gusmfs <oigostavo@gmail.com>
2023-11-04 03:32:24 -0300

    dsaudha

--- src/routes/realEstate.routes.ts
+++ src/routes/realEstate.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { verifyAdmin, verifyBody, verifyToken } from "../middlewares/globals.middleware";
 import { verifyAddressExists } from "../middlewares/realEstates.middleware";
+import { createRealEstateController } from "../controller/realEstate.controller";
 
 export const realEstateRoutes : Router = Router()
-realEstateRoutes.post('/', verifyToken, verifyAdmin, verifyBody, verifyAddressExists)
+realEstateRoutes.post('/', verifyToken, verifyAdmin, verifyBody, verifyAddressExists, createRealEstateController)
 realEstateRoutes.get('/')
