0a740fe7bc4e10e452fc682f1790014bb915a7e0
gusmfs <oigostavo@gmail.com>
2023-11-04 03:29:06 -0300

    dsaudha

--- src/controller/realEstate.controller.ts
+++ src/controller/realEstate.controller.ts
@@ -4,5 +4,4 @@ import { createRealEstateService } from "../services/realEstate.service";
 export const createRealEstateController = async (req: Request, res: Response) : Promise<Response> => {
     const realEstate = await createRealEstateService(req.body)
     return res.status(201).json(realEstate)
-    
 }
