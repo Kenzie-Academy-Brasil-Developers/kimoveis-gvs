70cd8495534022e8fe26fdf4d2dc5cce83867385
gusmfs <oigostavo@gmail.com>
2023-11-05 03:11:49 -0300

    feat sched serv

---
+++ src/controller/schedules.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { createScheduleService } from "../services/schedules.service";
+
+export const  createScheduleController = async(req: Request, res: Response): Promise<Response> => {
+    const {sub} = res.locals.decoded
+    await createScheduleService(req.body, sub)
+    return res.status(200).json({message: 'Schedule created'})
+}                                                      
