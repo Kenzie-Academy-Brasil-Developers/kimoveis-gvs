4fa66f23a54b8acd38121c903282c60501b9ac32
gusmfs <oigostavo@gmail.com>
2023-11-05 03:26:10 -0300

    feat sched serv

--- src/routes/schedules.routes.ts
+++ src/routes/schedules.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { verifyAdmin, verifyBody, verifyToken } from "../middlewares/globals.middleware";
 import { verifyRealEstatesExist, verifyRealEstatesSchedulesExist, verifyUserScheduleExists } from "../middlewares/schedules.middleware";
+import { createScheduleController, readAllScheduleRealEstateController } from "../controller/schedules.controller";
 
 export const schedulesRoutes : Router = Router()
-schedulesRoutes.post('/', verifyToken ,verifyBody, verifyRealEstatesExist, verifyRealEstatesSchedulesExist, verifyUserScheduleExists)
-schedulesRoutes.get('/realEstate/:id', verifyToken, verifyAdmin)
+schedulesRoutes.post('/', verifyToken ,verifyBody, verifyRealEstatesExist, verifyRealEstatesSchedulesExist, verifyUserScheduleExists, createScheduleController)
+schedulesRoutes.get('/realEstate/:id', verifyToken, verifyAdmin, readAllScheduleRealEstateController)
